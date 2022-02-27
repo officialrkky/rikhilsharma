1c6853242e70e75e3be17e4df76c1831b9f9d536
officialrkky <93412800+officialrkky@users.noreply.github.com>
2022-02-27 16:23:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
 $(document).ready(() => {
-  $('.activeAboutMe').on('click', () => {
+  $('#activeInformation').on('click', () => {
+    $('#information').show();
+    $('#experience').hide()
+    $('#education').hide()
+    $('#interest_and_hobbies').hide()
+    $('#honors_and_awards').hide()
+    $('#skills').hide()
+  })
+  
+  $('#activeExperience').on('click', () => {
     $('#information').hide();
+    $('#experience').show()
+    $('#education').hide()
+    $('#interest_and_hobbies').hide()
+    $('#honors_and_awards').hide()
+    $('#skills').hide()
+  })
+})
+
+  $('#activeEducation').on('click', () => {
+    $('#information').hide();
+    $('#experience').hide()
+    $('#education').show()
+    $('#interest_and_hobbies').hide()
+    $('#honors_and_awards').hide()
+    $('#skills').hide()
+  })
+})
+
+  $('#activeInterestAndHobbies').on('click', () => {
+    $('#information').hide();
+    $('#experience').hide()
+    $('#education').hide()
+    $('#interest_and_hobbies').show()
+    $('#honors_and_awards').hide()
+    $('#skills').hide()
+  })
+})
+
+  $('#activeHonorsAndAwards').on('click', () => {
+    $('#information').hide();
+    $('#experience').hide()
+    $('#education').hide()
+    $('#interest_and_hobbies').hide()
+    $('#honors_and_awards').show()
+    $('#skills').hide()
+  })
+})
+
+  $('#activeSkills').on('click', () => {
+    $('#information').hide();
+    $('#experience').hide()
+    $('#education').hide()
+    $('#interest_and_hobbies').hide()
+    $('#honors_and_awards').hide()
+    $('#skills').show()
   })
 })
